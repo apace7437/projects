142af1c463634de7874b76e294d1a24fa9084e51
apace7437 <apace7437@sohamcollege.org.uk>
2015-03-17 12:08:20 +0000

    17/3/15

--- speedCamera.py
+++ speedCamera.py
@@ -1,15 +1,20 @@
-import time, json, math, RPi.GPIO as GPIO
+import datetime, json, math, RPi.GPIO as GPIO
 
 def saveSpeeders():
     speeders
 
-t1 = int(input("What is the time the car enters the monitered zone (secs)?"))
-t2 = int(input("What is the time the car exits the monitered zone (secs)?"))
+t1 = datetime.datetime.utcnow()
+t2 = datetime.datetime.utcnow()
 
 distance = 1
 
-speed = distance/(t2-t1)
+speed = distance/abs(t2-t1)
 
 print(str(speed) + "miles/min")
-    
-speeders = speed > 1
+
+speeders = speed > 60
+
+numPlates = ["AD54SGY","YZ76YSE","ZA90ABC","AAAAAAA","JDHVB76","AB12CIA"] 
+with open("numPlates.txt","w") as numPlates_file:
+    for plate in numPlates:
+        numPlates_file.write(plate+"\n")
